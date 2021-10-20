9837fa2f2c6cd4fdd613f6dc65487386f21477d0
Ansgar Mertens <ansgar@hashicorp.com>
2021-10-20 18:01:16 +0200

    chore(deps): upgrade to cdktf@0.7.0-pre.382 for token system fix

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,7 +2,7 @@ import { CdktfAwsCdkProject } from "./projen";
 
 const project = new CdktfAwsCdkProject({
   terraformProvider: "aws@~> 3.0",
-  cdktfVersion: '0.7',
+  cdktfVersion: '0.7.0-pre.382',
   constructsVersion: "^10.0.0",
   minNodeVersion: "12.19.0",
   projenrcTs: true
