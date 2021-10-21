bc0cf5c47e758a002aa58e2a571d5520fb4d87b0
Ansgar Mertens <ansgar@hashicorp.com>
2021-10-21 17:33:21 +0200

    feat: test examples when building

--- projen/index.ts
+++ projen/index.ts
@@ -87,9 +87,16 @@ export class CdktfAwsCdkProject extends JsiiProject {
       exec: `ts-node --skip-project --compiler-options '{"target":"es6"}' .projenrc.ts`,
     });
 
-    [this.tasks.tryFind("compile"), this.tasks.tryFind("test")].forEach(
-      (task) => task.env("NODE_OPTIONS", "--max-old-space-size=7516")
+    [this.compileTask, this.testTask].forEach((task) =>
+      task.env("NODE_OPTIONS", "--max-old-space-size=7516")
     );
+
+    const testExamples = this.addTask("examples:test", {
+      cwd: "examples/typescript-cron-lambda",
+      exec: "yarn test:local",
+    });
+
+    this.buildTask.spawn(testExamples);
 
     // for local developing (e.g. linking local changes to cdktf)
     this.addGitIgnore(".yalc");
