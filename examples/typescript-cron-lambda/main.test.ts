723abdb764d6610958c2e391c9f70a134889e9a4
Ansgar Mertens <ansgar@hashicorp.com>
2021-11-12 07:47:50 +0100

    fix: update snapshots for cron lamba example

--- examples/typescript-cron-lambda/main.test.ts
+++ examples/typescript-cron-lambda/main.test.ts
@@ -28,7 +28,7 @@ describe("typescript-cron-lambda", () => {
               \\"depends_on\\": [
                 \\"time_sleep.adapter_lambdaServiceRole494E4CA6_sleep_lambdaServiceRole494E4CA6_73847BDD\\"
               ],
-              \\"desired_state\\": \\"{\\\\\\"Code\\\\\\":{\\\\\\"ZipFile\\\\\\":\\\\\\"def main(event, context):\\\\\\\\n    print(\\\\\\\\\\\\\\"I'm running!\\\\\\\\\\\\\\")\\\\\\"},\\\\\\"Role\\\\\\":\\\\\\"\${aws_iam_role.adapter_lambdaServiceRole494E4CA6_7D4D29EC.arn}\\\\\\",\\\\\\"Handler\\\\\\":\\\\\\"index.main\\\\\\",\\\\\\"Runtime\\\\\\":\\\\\\"python3.6\\\\\\",\\\\\\"Timeout\\\\\\":300}\\",
+              \\"desired_state\\": \\"\${jsonencode({Code = {ZipFile = \\\\\\"def main(event, context):\\\\\\\\n    print(\\\\\\\\\\\\\\"I'm running!\\\\\\\\\\\\\\")\\\\\\"}, Role = aws_iam_role.adapter_lambdaServiceRole494E4CA6_7D4D29EC.arn, Handler = \\\\\\"index.main\\\\\\", Runtime = \\\\\\"python3.6\\\\\\", Timeout = 300})}\\",
               \\"type_name\\": \\"AWS::Lambda::Function\\"
             }
           },
@@ -46,7 +46,7 @@ describe("typescript-cron-lambda", () => {
           },
           \\"aws_iam_role\\": {
             \\"adapter_lambdaServiceRole494E4CA6_7D4D29EC\\": {
-              \\"assume_role_policy\\": \\"{\\\\\\"Statement\\\\\\":[{\\\\\\"Action\\\\\\":\\\\\\"sts:AssumeRole\\\\\\",\\\\\\"Effect\\\\\\":\\\\\\"Allow\\\\\\",\\\\\\"Principal\\\\\\":{\\\\\\"Service\\\\\\":\\\\\\"lambda.amazonaws.com\\\\\\"}}],\\\\\\"Version\\\\\\":\\\\\\"2012-10-17\\\\\\"}\\",
+              \\"assume_role_policy\\": \\"\${jsonencode({Statement = [{Action = \\\\\\"sts:AssumeRole\\\\\\", Effect = \\\\\\"Allow\\\\\\", Principal = {Service = \\\\\\"lambda.amazonaws.com\\\\\\"}}], Version = \\\\\\"2012-10-17\\\\\\"})}\\",
               \\"managed_policy_arns\\": [
                 \\"\${join(\\\\\\"\\\\\\", [\\\\\\"arn:\\\\\\", data.aws_partition.adapter_aws-partition_5B16AD9D.partition, \\\\\\":iam::aws:policy/service-role/AWSLambdaBasicExecutionRole\\\\\\"])}\\"
               ]
