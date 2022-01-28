e1cc66d81b08c1dd56e6039043145ed55c975218
ansgarm <ansgarm@users.noreply.github.com>
2022-01-28 04:17:57 +0000

    chore: upgrade provider

---
+++ src/aws/imagebuilder/data-aws-imagebuilder-infrastructure-configurations.ts
@@ -0,0 +1,120 @@
+// generated from terraform resource schema
+
+import { Construct } from 'constructs';
+import * as cdktf from 'cdktf';
+
+/**
+* AWS Image Builder
+*/
+export interface DataAwsImagebuilderInfrastructureConfigurationsConfig extends cdktf.TerraformMetaArguments {
+  /**
+  * filter block
+  * 
+  * Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/d/imagebuilder_infrastructure_configurations.html#filter DataAwsImagebuilderInfrastructureConfigurations#filter}
+  */
+  readonly filter?: DataAwsImagebuilderInfrastructureConfigurationsFilter[];
+}
+export interface DataAwsImagebuilderInfrastructureConfigurationsFilter {
+  /**
+  * Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/d/imagebuilder_infrastructure_configurations.html#name DataAwsImagebuilderInfrastructureConfigurations#name}
+  */
+  readonly name: string;
+  /**
+  * Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/aws/d/imagebuilder_infrastructure_configurations.html#values DataAwsImagebuilderInfrastructureConfigurations#values}
+  */
+  readonly values: string[];
+}
+
+export function dataAwsImagebuilderInfrastructureConfigurationsFilterToTerraform(struct?: DataAwsImagebuilderInfrastructureConfigurationsFilter): any {
+  if (!cdktf.canInspect(struct)) { return struct; }
+  if (cdktf.isComplexElement(struct)) {
+    throw new Error("A complex element was used as configuration, this is not supported: https://cdk.tf/complex-object-as-configuration");
+  }
+  return {
+    name: cdktf.stringToTerraform(struct!.name),
+    values: cdktf.listMapper(cdktf.stringToTerraform)(struct!.values),
+  }
+}
+
+
+/**
+* Represents a {@link https://www.terraform.io/docs/providers/aws/d/imagebuilder_infrastructure_configurations.html aws_imagebuilder_infrastructure_configurations}
+*/
+export class DataAwsImagebuilderInfrastructureConfigurations extends cdktf.TerraformDataSource {
+
+  // =================
+  // STATIC PROPERTIES
+  // =================
+  public static readonly tfResourceType: string = "aws_imagebuilder_infrastructure_configurations";
+
+  // ===========
+  // INITIALIZER
+  // ===========
+
+  /**
+  * Create a new {@link https://www.terraform.io/docs/providers/aws/d/imagebuilder_infrastructure_configurations.html aws_imagebuilder_infrastructure_configurations} Data Source
+  *
+  * @param scope The scope in which to define this construct
+  * @param id The scoped construct ID. Must be unique amongst siblings in the same scope
+  * @param options DataAwsImagebuilderInfrastructureConfigurationsConfig = {}
+  */
+  public constructor(scope: Construct, id: string, config: DataAwsImagebuilderInfrastructureConfigurationsConfig = {}) {
+    super(scope, id, {
+      terraformResourceType: 'aws_imagebuilder_infrastructure_configurations',
+      terraformGeneratorMetadata: {
+        providerName: 'aws'
+      },
+      provider: config.provider,
+      dependsOn: config.dependsOn,
+      count: config.count,
+      lifecycle: config.lifecycle
+    });
+    this._filter = config.filter;
+  }
+
+  // ==========
+  // ATTRIBUTES
+  // ==========
+
+  // arns - computed: true, optional: false, required: false
+  public get arns() {
+    return this.getListAttribute('arns');
+  }
+
+  // id - computed: true, optional: true, required: false
+  public get id() {
+    return this.getStringAttribute('id');
+  }
+
+  // names - computed: true, optional: false, required: false
+  public get names() {
+    return this.getListAttribute('names');
+  }
+
+  // filter - computed: false, optional: true, required: false
+  private _filter?: DataAwsImagebuilderInfrastructureConfigurationsFilter[]; 
+  public get filter() {
+    // Getting the computed value is not yet implemented
+    return this.interpolationForAttribute('filter') as any;
+  }
+  public set filter(value: DataAwsImagebuilderInfrastructureConfigurationsFilter[]) {
+    this._filter = value;
+  }
+  public resetFilter() {
+    this._filter = undefined;
+  }
+  // Temporarily expose input value. Use with caution.
+  public get filterInput() {
+    return this._filter;
+  }
+
+  // =========
+  // SYNTHESIS
+  // =========
+
+  protected synthesizeAttributes(): { [name: string]: any } {
+    return {
+      filter: cdktf.listMapper(dataAwsImagebuilderInfrastructureConfigurationsFilterToTerraform)(this._filter),
+    };
+  }
+}
