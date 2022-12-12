031d115730d2c1d3cf0988f13d63d91a59db96cf
Nara Kasbergen <nara.kasbergen@gmail.com>
2022-12-12 12:32:34 +0100

    fix: regenerate stale workflow to take advantage of new options
    Newer versions of Projen automatically configure this to add
    support for a "backlog" label on which this workflow won't run.
    This allows us to avoid situations where a maintainer has to
    continually remove the "stale" label on issues we consider valid
    but haven't had time to work on yet.
    
    This also adds a lock workflow in keeping with best practices from
    our other open source projects.

---
+++ projen/lock-issues.ts
@@ -0,0 +1,48 @@
+import { javascript } from "projen";
+import { JobPermission } from "projen/lib/github/workflows-model";
+
+/**
+ * Automatically locks issues and PRs after 7 days. Note that 90% of the issues and PRs
+ * on these repos are automated builds and notifications, so locking these threads is
+ * generally not expected to cause issues.
+ */
+export class LockIssues {
+  constructor(project: javascript.NodeProject) {
+    const workflow = project.github?.addWorkflow("lock");
+
+    if (!workflow) throw new Error("no workflow defined");
+
+    workflow.on({
+      schedule: [{ cron: "20 2 * * *" }],
+    });
+
+    workflow.addJob("lock", {
+      runsOn: ["ubuntu-latest"],
+      permissions: {
+        pullRequests: JobPermission.WRITE,
+        issues: JobPermission.WRITE,
+      },
+      steps: [
+        {
+          uses: "dessant/lock-threads@v3.0.0",
+          with: {
+            "issue-comment":
+              `I'm going to lock this issue because it has been closed for at least 30 days.
+              This helps our maintainers find and focus on the active issues.
+              If you've found a problem that seems similar to this, please 
+              [open a new issue](https://github.com/hashicorp/cdktf-aws-cdk/issues/new) 
+              so we can investigate further.`.replace(/\s+/g, " "),
+            "issue-inactive-days": 30,
+            "pr-comment":
+              `I'm going to lock this pull request because it has been closed for at least 30 days.
+              This helps our maintainers find and focus on the active issues.
+              If you've found a problem that seems related to this change, please 
+              [open a new issue](https://github.com/hashicorp/cdktf-aws-cdk/issues/new) 
+              so we can investigate further.`.replace(/\s+/g, " "),
+            "pr-inactive-days": 30,
+          },
+        },
+      ],
+    });
+  }
+}
