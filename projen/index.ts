3cefcffa4e874d0e69cb4b7be5d04840ae691314
Ansgar Mertens <ansgar@hashicorp.com>
2023-03-02 15:26:17 +0100

    feat: make adding copyright headers part of the build workflow running in PRs

--- projen/index.ts
+++ projen/index.ts
@@ -215,6 +215,15 @@ export class CdktfAwsCdkProject extends cdk.JsiiProject {
     upgrade.steps.splice(1, 0, setSafeDirectory);
     pr.steps.splice(1, 0, setSafeDirectory);
 
+    this.buildWorkflow?.addPostBuildSteps(
+      {
+        name: "Setup Copywrite tool",
+        uses: "hashicorp/setup-copywrite@3ace06ad72e6ec679ea8572457b17dbc3960b8ce", // v1.0.0
+        with: { token: "${{ secrets.GITHUB_TOKEN }}" },
+      },
+      { name: "Add headers using Copywrite tool", run: "copywrite headers" }
+    );
+
     new CdktfConfig(this, {
       terraformProvider,
       providerName,
