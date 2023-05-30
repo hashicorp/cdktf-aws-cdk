b8e3fad50a91ffc91a1ea3ee88a843881f22b40e
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2023-05-30 15:36:54 +0200

    chore: pin additional GitHub Actions versions

--- .projenrc.ts
+++ .projenrc.ts
@@ -8,12 +8,13 @@ import { CdktfAwsCdkProject } from "./projen";
 const githubActionPinnedVersions = {
   "actions/checkout": "8e5e7e5ab8b370d6c329ec480221332ada57f0ab", // v3.5.2
   "actions/upload-artifact": "0b7f8abb1508181956e8e162db84b466c27e18ce", // v3.1.2
+  "actions/download-artifact": "9bc31d5ccc31df68ecc42ccf4149144866c47d8a", // v3.0.2
   "dessant/lock-threads": "c1b35aecc5cdb1a34539d14196df55838bb2f836", // v4.0.0
   "amannn/action-semantic-pull-request":
     "c3cd5d1ea3580753008872425915e343e351ab54", // v5.2.0
   "actions/setup-node": "64ed1c7eab4cce3362f8c340dee64e5eaeef8f7c", // v3.6.0
-  "actions/stale": "a20b814fb01b71def3bd6f56e7494d667ddf28da", // v4.1.1
-  "peter-evans/create-pull-request": "2b011faafdcbc9ceb11414d64d0573f37c774b04", // v4.2.3
+  "actions/stale": "1160a2240286f5da8ec72b1c0816ce2481aabf84", // v8.0.0
+  "peter-evans/create-pull-request": "284f54f989303d2699d373481a0cfa13ad5a6666", // v5.0.1
   "pascalgn/automerge-action": "22948e0bc22f0aa673800da838595a3e7347e584", // v0.15.6
 };
 
