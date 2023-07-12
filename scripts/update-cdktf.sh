3b327993a88a4877347e60f18d3a9c0f09ff8996
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2023-07-12 10:37:08 +0200

    chore: do not update examples as part of script
    This does not currently work because the examples have `@cdktf/aws-cdk` as a dependency which have a peer dependency on specific versions of `cdktf` and thus subsequent installs of `yarn install` / `npm install` complain. However, now that we have Dependabot set up to manage dependency updates for the examples, Dependabot will create PRs to update these.

--- scripts/update-cdktf.sh
+++ scripts/update-cdktf.sh
@@ -13,14 +13,4 @@ yarn
 sed -i "s/cdktfVersion: ".*",/cdktfVersion: \"$CDKTF_VERSION\",/" "$PROJECT_ROOT/.projenrc.ts"
 npx projen
 
-echo "Updating examples"
-# Loop through all examples and update the cdktf version
-for example in $(find "$PROJECT_ROOT/examples" -mindepth 1 -maxdepth 1 -type d); do
-  echo "Updating example $example"
-  cd "$example"
-  yarn
-  yarn add -D cdktf-cli@$CDKTF_VERSION
-  yarn add cdktf@$CDKTF_VERSION
-done
-
 echo "Done"
