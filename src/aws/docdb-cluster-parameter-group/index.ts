6be77d8844df300cc1b858b6bab05307a61fac1b
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-08-17 21:19:08 +0000

    feat: upgrade providers
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/aws/docdb-cluster-parameter-group/index.ts
+++ src/aws/docdb-cluster-parameter-group/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/aws/3.76.1/docs/resources/docdb_cluster_parameter_group
 // generated from terraform resource schema
 
