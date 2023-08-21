dadf91fc16b5d2748ea294ffd92953e5ed2e9dfd
Nara Kasbergen <nara.kasbergen@gmail.com>
2023-08-21 12:48:14 +0200

    chore: attempt to fix broken upgrade workflow

--- projen/upgrade-cdktf.ts
+++ projen/upgrade-cdktf.ts
@@ -40,21 +40,23 @@ export class UpgradeCDKTF {
             id: "current_version",
             run: [
               `OLD_VERSION=$(npm list cdktf --depth=0 --json | jq -r '.dependencies.cdktf.version')`,
+              `OLD_VERSION_SHORT=$(cut -d "." -f 2 <<< "$OLD_VERSION")`,
               `echo "value=$OLD_VERSION" >> $GITHUB_OUTPUT`,
-              `echo 'short=$(cut -d "." -f 2 <<< "$OLD_VERSION")' >> $GITHUB_OUTPUT`,
+              `echo "short=$OLD_VERSION_SHORT" >> $GITHUB_OUTPUT`,
             ].join("\n"),
-            // NOTE: No, there is no good way to do this in Yarn, until we upgrade to Yarn 2+ (see below)
+            // NOTE: No, there is no good way to do this in Yarn, until (if) we upgrade to Yarn 2+ (see below)
           },
           {
             name: "Get latest CDKTF version",
             id: "latest_version",
             run: [
               `CDKTF_VERSION=$(yarn info cdktf --json | jq -r '.data.version')`,
+              `CDKTF_VERSION_SHORT=$(cut -d "." -f 2 <<< "$CDKTF_VERSION")`,
               `echo "value=$CDKTF_VERSION" >> $GITHUB_OUTPUT`,
-              `echo 'short=$(cut -d "." -f 2 <<< "$CDKTF_VERSION")' >> $GITHUB_OUTPUT`,
+              `echo "short=$CDKTF_VERSION_SHORT" >> $GITHUB_OUTPUT`,
             ].join("\n"),
-            // IMPORTANT: the above behavior will change in Yarn 2+; `yarn info` will instead give the version of the installed package
-            // When we upgrade we'll likely want to switch to `yarn npm info`: https://yarnpkg.com/cli/npm/info
+            // IMPORTANT: the above behavior changed in Yarn 2+; `yarn info` instead gives the version of the installed package
+            // If/when we upgrade we'll likely want to switch to `yarn npm info`: https://yarnpkg.com/cli/npm/info
           },
           {
             name: "Close old PR",
