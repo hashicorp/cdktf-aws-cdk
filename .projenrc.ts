2fe3f11d7448beff300383bae8b761b6152353bf
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-08-24 05:36:27 +0000

    chore!: increase minimum supported Node.js version to 18
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -24,7 +24,7 @@ const project = new CdktfAwsCdkProject({
   cdktfVersion: "0.18.0",
   constructsVersion: "^10.0.25",
   jsiiVersion: "^5.1.0",
-  minNodeVersion: "16.14.0",
+  minNodeVersion: "18.12.0",
   projenrcTs: true,
 });
 
