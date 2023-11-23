b5c0bd359a6d7366478ff791f7527b6744f17c57
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2023-11-23 20:45:41 +0100

    chore(deps): remove dependency on node-fetch (#542)
    Continuation of the work started in #414 that couldn't be merged at the
    time because we needed to wait for actions/github-script to be upgraded
    to the new Node 20 runner instead.

--- .projenrc.ts
+++ .projenrc.ts
@@ -6,15 +6,15 @@
 import { CdktfAwsCdkProject } from "./projenrc";
 
 const githubActionPinnedVersions = {
-  "actions/checkout": "8ade135a41bc03ea155e62e844d188df1ea18608", // v4.1.0
+  "actions/checkout": "b4ffde65f46336ab88eb53be808477a3936bae11", // v4.1.1
   "actions/download-artifact": "9bc31d5ccc31df68ecc42ccf4149144866c47d8a", // v3.0.2
-  "actions/github-script": "d7906e4ad0b1822421a7e6a35d5ca353c962f410", // v6.4.1
-  "actions/setup-node": "5e21ff4d9bc1a8cf6de233a3057d20ec6b3fb69d", // v3.8.1
+  "actions/github-script": "60a0d83039c74a4aee543508d2ffcb1c3799cdea", // v7.0.1
+  "actions/setup-node": "8f152de45cc393bb48ce5d89d36b731f54556e65", // v4.0.0
   "actions/stale": "1160a2240286f5da8ec72b1c0816ce2481aabf84", // v8.0.0
   "actions/upload-artifact": "a8a3f3ad30e3422c9c7b888a15615d19a852ae32", // v3.1.3
   "amannn/action-semantic-pull-request":
-    "47b15d52c5c30e94a17ec87eb8dd51ff5221fed9", // v5.3.0
-  "dessant/lock-threads": "be8aa5be94131386884a6da4189effda9b14aa21", // v4.0.1
+    "e9fabac35e210fea40ca5b14c0da95a099eff26f", // v5.4.0
+  "dessant/lock-threads": "d42e5f49803f3c4e14ffee0378e31481265dda22", // v5.0.0
   "peter-evans/create-pull-request": "153407881ec5c347639a548ade7d8ad1d6740e38", // v5.0.2
 };
 
