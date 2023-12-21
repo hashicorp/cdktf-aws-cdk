9ab8ed248685a0c723cf6bbfdc001df8754b3db2
Nara Kasbergen <nara.kasbergen@gmail.com>
2023-12-21 13:17:16 +0100

    chore(ci): update actions/stale workflow

--- .projenrc.ts
+++ .projenrc.ts
@@ -10,7 +10,7 @@ const githubActionPinnedVersions = {
   "actions/download-artifact": "9bc31d5ccc31df68ecc42ccf4149144866c47d8a", // v3.0.2
   "actions/github-script": "60a0d83039c74a4aee543508d2ffcb1c3799cdea", // v7.0.1
   "actions/setup-node": "8f152de45cc393bb48ce5d89d36b731f54556e65", // v4.0.0
-  "actions/stale": "1160a2240286f5da8ec72b1c0816ce2481aabf84", // v8.0.0
+  "actions/stale": "28ca1036281a5e5922ead5184a1bbf96e5fc984e", // v9.0.0
   "actions/upload-artifact": "a8a3f3ad30e3422c9c7b888a15615d19a852ae32", // v3.1.3
   "amannn/action-semantic-pull-request":
     "e9fabac35e210fea40ca5b14c0da95a099eff26f", // v5.4.0
