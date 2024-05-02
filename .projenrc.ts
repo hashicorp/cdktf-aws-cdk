f90f96871d7cb7cfa599022456139e1281b26b4f
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2024-05-02 15:19:03 +0200

    chore(deps): update pinned versions of GitHub Actions (#751)

--- .projenrc.ts
+++ .projenrc.ts
@@ -6,16 +6,16 @@
 import { CdktfAwsCdkProject } from "./projenrc";
 
 const githubActionPinnedVersions = {
-  "actions/checkout": "b4ffde65f46336ab88eb53be808477a3936bae11", // v4.1.1
-  "actions/download-artifact": "c850b930e6ba138125429b7e5c93fc707a7f8427", // v4.1.4
+  "actions/checkout": "0ad4b8fadaa221de15dcec353f45205ec38ea70b", // v4.1.4
+  "actions/download-artifact": "65a9edc5881444af0b9093a5e628f2fe47ea3b2e", // v4.1.7
   "actions/github-script": "60a0d83039c74a4aee543508d2ffcb1c3799cdea", // v7.0.1
   "actions/setup-node": "60edb5dd545a775178f52524783378180af0d1f8", // v4.0.2
   "actions/stale": "28ca1036281a5e5922ead5184a1bbf96e5fc984e", // v9.0.0
-  "actions/upload-artifact": "5d5d22a31266ced268874388b861e4b58bb5c2f3", // v4.3.1
+  "actions/upload-artifact": "65462800fd760344b1a7b4382951275a0abb4808", // v4.3.3
   "amannn/action-semantic-pull-request":
-    "e9fabac35e210fea40ca5b14c0da95a099eff26f", // v5.4.0
+    "cfb60706e18bc85e8aec535e3c577abe8f70378e", // v5.5.2
   "dessant/lock-threads": "1bf7ec25051fe7c00bdd17e6a7cf3d7bfb7dc771", // v5.0.1
-  "peter-evans/create-pull-request": "70a41aba780001da0a30141984ae2a0c95d8704e", // v6.0.2
+  "peter-evans/create-pull-request": "6d6857d36972b65feb161a90e484f2984215f83e", // v6.0.5
 };
 
 const project = new CdktfAwsCdkProject({
