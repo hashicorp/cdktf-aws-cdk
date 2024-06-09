5c2edb94c525194a72de24cd9fc30a9be89e2a65
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-06-09 21:13:05 +0000

    feat: upgrade providers
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/aws/route53-vpc-association-authorization/index.ts
+++ src/aws/route53-vpc-association-authorization/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/aws/3.76.1/docs/resources/route53_vpc_association_authorization
 // generated from terraform resource schema
 
