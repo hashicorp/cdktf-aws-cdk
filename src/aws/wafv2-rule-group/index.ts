3dc50e2fe40ba3569537216e621ddd2ed5f99722
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-10-20 21:22:41 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/aws/wafv2-rule-group/index.ts
+++ src/aws/wafv2-rule-group/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/aws/3.76.1/docs/resources/wafv2_rule_group
 // generated from terraform resource schema
 
