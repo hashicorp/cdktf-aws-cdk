f7ce15248d891067fe12a934f3761e077d20d7c6
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-10-20 21:13:18 +0000

    feat: upgrade providers
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/aws/s3-bucket-public-access-block/index.ts
+++ src/aws/s3-bucket-public-access-block/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/aws/3.76.1/docs/resources/s3_bucket_public_access_block
 // generated from terraform resource schema
 
--- src/time/sleep/index.ts
+++ src/time/sleep/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/time/0.7.2/docs/resources/sleep
 // generated from terraform resource schema
 
